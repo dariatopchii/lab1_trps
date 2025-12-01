3dcde66d8968d57b09bc6164e54932c4dda89ce7
NureTopchiiDaria <dar.topchii@gmail.com>
2025-12-01 17:42:22 +0100

    first commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculatorLib;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+
+    public int Sub(int a, int b) => a - b;
+
+    public int Mul(int a, int b) => a * b;
+
+    public int Div(int a, int b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException();
+        return a / b;
+    }
+
+    public bool IsEven(int x) => x % 2 == 0;
+}
